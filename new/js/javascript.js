b9037520817553574399448736bce418e93d4c4b
Derjojooo <49856179+Derjojooo@users.noreply.github.com>
2020-04-15 13:58:33 +0200

    js cleanup

--- new/js/javascript.js
+++ new/js/javascript.js
@@ -5,14 +5,6 @@ script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-// Mouse Circle
-
-$(document).mousemove(function(e) {
-  $("#mouse_circle").css({
-    left: e.clientX,
-    top: e.clientY
-  });
-});
 
 //Topic Preview
 
