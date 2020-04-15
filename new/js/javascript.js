be312d1a61640e3eac8604a2c69c589ad25ceede
Derjojooo <49856179+Derjojooo@users.noreply.github.com>
2020-04-15 12:06:26 +0200

    update

--- new/js/javascript.js
+++ new/js/javascript.js
@@ -1,11 +1,3 @@
-
-
 $(document).mousemove(function(e){
     $("#mouse_circle").css({left:e.clientX, top:e.clientY});
 });
-
-document.getElementByClassName("daysoftype").onmouseover = mouseOver();
-
-function mouseOver() {
-  document.getElementByByClassName("header").style.background.color = "red";
-}
